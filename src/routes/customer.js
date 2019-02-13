f0740e5116733a33752b35a2d2f65f5c644e7b00
KUMA23 <m.kumar.1@elsevier.com>
2019-02-13 11:09:30 +0530

    CRUD API updates

--- src/routes/customer.js
+++ src/routes/customer.js
@@ -5,7 +5,6 @@ let router = express.Router();
 // Create a new customer - POST API
 router.post('/customer', (req, res) => {
     // save logic req.body
-    console.log(req.body)
     if (Object.keys(req.body).length === 0) {
         return res.status(400).send('Request body is missing!');
     }
@@ -21,6 +20,51 @@ router.post('/customer', (req, res) => {
         if (!doc || doc.length === 0) {
             return res.status(500).send(doc);
         }
+        res.status(201).send(doc);
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    });
+});
+
+// Get Customer Get API
+router.get('/customer', (req, res) => {
+    
+    CustomerModel.findOne({
+        email: req.query.email
+    })
+    .then(doc => {
+        res.status(201).send(doc);
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    });
+});
+
+// Update Customer - put
+router.put('/customer', (req, res) => {
+    // save logic req.body
+    if (Object.keys(req.body).length === 0) {
+        return res.status(400).send('Request body is missing!');
+    }
+
+    CustomerModel.findOneAndUpdate({
+        email: req.query.email
+    }, req.body, {new:true})
+    .then(doc => {        
+        res.status(201).send(doc);
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    });
+});
+
+// Delete a customer - delete api
+router.delete('/customer', (req, res) => {
+    CustomerModel.findOneAndUpdate({
+        email: req.query.email
+    })
+    .then(doc => {        
         res.status(201).send(doc);
     })
     .catch(err => {
